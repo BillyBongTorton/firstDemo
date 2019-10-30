b571abd6777a8692e61786a3ff45d0fb9b2304a2
BillyBongTorton <billybongtorton@gmail.com>
2019-10-30 21:49:00 +0600

    Painted pictures using Graph.py

---
+++ Pictures/pic.py
@@ -0,0 +1,21 @@
+from graph import *
+
+penColor("black")
+brushColor("yellow")
+
+circle(250, 250, 120)
+
+brushColor("red")
+circle(200, 215, 20)
+circle(300, 215, 18)
+
+brushColor('Black')
+circle(200, 215, 8)
+circle(300, 215, 8)
+
+rectangle(300, 290, 200, 310)
+penSize(10)
+line(235, 210, 130, 140)
+line(380, 160, 260, 205)
+
+run()
